3cc59b17d27407035a6b7dba809641a184a49973
Carlos Gonzalez <cagl18@gmail.com>
2019-09-05 20:55:09 -0400

    updated readme file

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   };
 
   componentDidMount() {
-    this.onSearchVideos('buildings');
+    this.onSearchVideos('coral reefs');
   }
 
   onSearchVideos = async term => {
