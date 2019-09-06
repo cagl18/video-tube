33f794bb75d44e55b0ec7c2f26b1da06faf1310a
Carlos Gonzalez <cagl18@gmail.com>
2019-09-05 20:42:22 -0400

    Updates

---
+++ precache-manifest.f03d9ed93d8a86765e614c32467dc82a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3277bfc7369083574c7239b734e93a87",
+    "url": "/videotube/index.html"
+  },
+  {
+    "revision": "7fbb6bedafeea8b24b18",
+    "url": "/videotube/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "ee7cc243191b03329aa2",
+    "url": "/videotube/static/js/2.f61fc0d8.chunk.js"
+  },
+  {
+    "revision": "7fbb6bedafeea8b24b18",
+    "url": "/videotube/static/js/main.2fe3c942.chunk.js"
+  },
+  {
+    "revision": "ae43184fc5f965db5930",
+    "url": "/videotube/static/js/runtime~main.0c49ed0d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/videotube/precache-manifest.f03d9ed93d8a86765e614c32467dc82a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/videotube/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
